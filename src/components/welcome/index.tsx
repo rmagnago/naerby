000bd4774de34245c545f4d66dbc4f5511d83088
Rafael Magnago <rmagnago@outlook.com>
2024-12-10 11:05:04 -0300

    NLW Pocket: aula 1

---
+++ src/components/welcome/index.tsx
@@ -0,0 +1,12 @@
+import { View, Image, Text } from 'react-native';
+import { s } from './styles';
+
+export function Welcome() {
+    return (
+        <View >
+            <Image source={require('@/assets/logo.png')} style={s.logo} />
+            <Text style={s.title}>Boas vindas ao Nearby!</Text>
+            <Text style={s.subtitle}>Tenha cupons de vantagens para usar em seus estabelecimentos preferidos</Text>
+        </View>
+    );
+}
